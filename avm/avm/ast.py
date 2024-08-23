a0991deaf2511e668bc6b61523a849d0c8525718
tieo <nutthawit1513@gmail.com>
2024-08-23 07:08:20 +0700

    change ASTNode to ASNode

--- avm/avm/ast.py
+++ avm/avm/ast.py
@@ -1,13 +1,13 @@
 import collections
 
 
-ASTNode = collections.namedtuple(
+ASNode = collections.namedtuple(
     'ASTNode', ['something_1', 'something_2', 'something_3'])
 
 
 class AST:
     """
-    The Abstract Syntax Tree use to convert user API to AST Node
+    The Abstract Syntax Tree use to convert user API to Set (Tree) of ASNode
     """
 
     pass

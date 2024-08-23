e2b782e4b286bc8b8bb4776bfe4cda9b58c18cb8
tieo <nutthawit1513@gmail.com>
2024-08-23 13:42:11 +0700

    add DmViewSet

--- avm/avm/urls.py
+++ avm/avm/urls.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+
+from dm import views as dm_views
+
+
+router = routers.DefaultRouter()
+router.register(r"dm", dm_views.DmViewSet, basename="dm")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
--- avm/dm/views.py
+++ avm/dm/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+
+class DmViewSet(viewsets.ViewSet):
+
+    def list(self, request):
+        return Response({"message": "Hello, World!"})

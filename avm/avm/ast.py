6da16d015dadac2e851634a70675be71a7ef5787
tieo <nutthawit1513@gmail.com>
2024-08-23 07:05:33 +0700

    add class AST for convert user API to ASTNode

---
+++ avm/avm/ast.py
@@ -0,0 +1,13 @@
+import collections
+
+
+ASTNode = collections.namedtuple(
+    'ASTNode', ['something_1', 'something_2', 'something_3'])
+
+
+class AST:
+    """
+    The Abstract Syntax Tree use to convert user API to AST Node
+    """
+
+    pass
